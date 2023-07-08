23b236bdc34d3bd1d434b065a11166884041cd9c
Guilherme <oguilhermefranco@gmail.com>
2023-07-08 19:18:11 -0300

    iDENTIDADE DE GENERO E PARENTESCO

--- src/main/java/com/example/tc/tech_challange/domain/ValidacaoException.java
+++
@@ -1,5 +0,0 @@
-package com.example.tc.tech_challange.domain;
-
-public class ValidacaoException extends RuntimeException {
-    public ValidacaoException(String mensagem) { super(mensagem); }
-}
--- src/main/java/com/example/tc/tech_challange/domain/endereco/DadosDetalhamentoEndereco.java
+++
@@ -1,9 +0,0 @@
-package com.example.tc.tech_challange.domain.endereco;
-
-public record DadosDetalhamentoEndereco(int cep, String rua, int numero, String compl,String bairro, String cidade, String estado) {
-
-    public DadosDetalhamentoEndereco(Endereco endereco){
-        this(endereco.getCep(),endereco.getRua(), endereco.getNumero(), endereco.getCompl(), endereco.getBairro(), endereco.getCidade(), endereco.getEstado());
-    }
-
-}
